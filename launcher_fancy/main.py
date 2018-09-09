b949bd8b943e211f08775c60ab7e5b6d783320a3
Andrew O'Rourke <andrew@montagne.uk>
2018-09-09 17:16:11 +0100

    Remove app.gif. Add WLAN strength.

--- launcher_fancy/main.py
+++ launcher_fancy/main.py
@@ -3,32 +3,22 @@
 ___name___         = "Fancy Launcher"
 ___license___      = "MIT"
 ___categories___   = ["System"]
-___dependencies___ = ["dialogs", "app", "sleep", "ugfx_helper"]
+___dependencies___ = ["dialogs", "wifi", "app", "sleep", "ugfx_helper"]
 ___launchable___   = False
-___bootstrapped___ = True
+___bootstrapped___ = False
 
-import ugfx_helper, ugfx
-import buttons
+import ugfx_helper, ugfx, buttons, wifi, time
 from app import *
 from dialogs import *
 
-ugfx_helper.init()
-
-apps = get_apps()
-cursor_x = 0
-cursor_y = 0
-
 def redraw_home():
     ugfx.clear()
-    time = ugfx.Label(90, 0, 60, 16, "13:37", justification=ugfx.Label.CENTER)
-    #default_image = ugfx.Image("launcher_fancy/app.gif")
 
     for y in range(3):
         for x in range(3):
             i = (y * 3) + x
             root_x = 16 + (74 * x)
             root_y = 30 + (91 * y)
-            #ugfx.display_image(root_x, root_y, default_image)
             ugfx.fill_circle(root_x + 30, root_y + 30, 30, ugfx.BLACK)
             ugfx.fill_circle(root_x + 30, root_y + 30, 25, ugfx.WHITE)
             app_title = apps[i].title
@@ -43,15 +33,38 @@ def draw_cursor(undraw):
     root_y = 30 + (91 * cursor_y)
     ugfx.box(root_x - 4, root_y - 4, 68, 88, colour)
 
+def draw_wlan():
+    if (wifi.is_connected()):
+        strength = wifi.get_strength()
+        if(strength is not None):
+            wlan_label.text("%02i" % (strength))
+    else:
+        wlan_label.text("-")
+
+def draw_pagination():
+
+ugfx_helper.init()
+
+apps = get_apps()
+cursor_x = 1
+cursor_y = 1
+page_number = 0
+page_count = len(apps) / 9
+if len(apps) % 9:
+    page_count += 1
+wlan_label = ugfx.Label(2, 0, 60, 16, "-", justification=ugfx.Label.LEFT)
+
 ugfx.power_mode(ugfx.POWER_SLEEP)
 redraw_home()
 ugfx.power_mode(ugfx.POWER_ON)
+last_update = time.ticks_ms()
 
 while True:
-    sleep.wfi()
+    sleep.sleep_ms(50)
+
     if buttons.is_triggered(Buttons.BTN_Menu):
         app.restart_to_default()
-    if buttons.is_triggered(Buttons.BTN_A):
+    if buttons.is_triggered(Buttons.BTN_B):
         redraw_home()
     if buttons.is_triggered(Buttons.BTN_1):
         apps[0].boot()
@@ -87,6 +100,12 @@ while True:
         draw_cursor(True)
         cursor_y -= 1
         draw_cursor(False)
-    if buttons.is_triggered(Buttons.JOY_Center):
+    if buttons.is_triggered(Buttons.JOY_Center) or buttons.is_triggered(Buttons.BTN_A):
         i = (cursor_y * 3) + cursor_x
         apps[i].boot()
+
+    if (time.ticks_ms() - last_update > 1000):
+        last_update = time.ticks_ms()
+        if (!wifi.is_connected()):
+            wifi.connect(wait=False, prompt_on_fail=False)
+        draw_wlan()
